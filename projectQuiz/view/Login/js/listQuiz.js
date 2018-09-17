5ff3085c2ffad15d7e41d7a8652c9082ae25aca5
Andressa Rodrigues <andressaf.rodrigues@hotmail.com>
2018-09-17 18:12:23 -0300

    Correção na listagem

--- projectQuiz/view/Login/js/listQuiz.js
+++ projectQuiz/view/Login/js/listQuiz.js
@@ -1,13 +1,10 @@
   jQuery(document).ready(function(){
-    jQuery('#tabela').empty();
+    jQuery('#tableQuiz').empty();
     jQuery.ajax({
       type:'POST',
       dataType: 'json',
       url: '../../../action/quizAction.php?require=2',
       success: function(data){
-        console.log(data.resp);
-        console.log(data.resp.length);
-        console.log(data.resp[1].desc);
         for (var i = data.resp.length - 1; i >= 0; i--) {
           jQuery('#tableQuiz').append('<tr> '+
                                         '<td>'+data.resp[i].id+'</td>'+
